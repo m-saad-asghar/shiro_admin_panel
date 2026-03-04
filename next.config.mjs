ffbeff859b93fe609adb24d70aa5a5db20410d49
Muhammad Saad Asghar <saadasghar3606@gmail.com>
2026-03-04 13:43:15 +0400

    change next

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,30 @@
 /** @type {import('next').NextConfig} */
 const config = {
-  eslint: { ignoreDuringBuilds: true },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+
+  // output: 'export',
+
   images: {
+    // unoptimized: true,
     remotePatterns: [
-      { protocol: 'http', hostname: 'localhost' },
-      { protocol: 'http', hostname: '127.0.0.1' },
-      { protocol: 'https', hostname: '**' },
-      { protocol: 'http', hostname: '**' },
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+      },
+      {
+        protocol: 'http',
+        hostname: '127.0.0.1',
+      },
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+      {
+        protocol: 'http',
+        hostname: '**',
+      },
     ],
   },
 };
